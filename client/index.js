ccf0bbff76eb4d6837178b5b712ef5865e99e0f8
kieran-tahir <kierantahir@gmail.com>
2021-09-23 10:47:39 +1200

    auth0 works

--- client/index.js
+++ client/index.js
@@ -10,10 +10,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Auth0Provider
-      domain='gardenz.au.auth0.com'
-      clientId='UPX21QATLKQlu9iMNfWmZd24Y3Xk2ezF'
+      domain='dev-6s8lzyu9.us.auth0.com'
+      clientId='DQ9jrSrXGYBRPuiayM4LzGnZF6bir3IN'
       redirectUri={window.location.origin}
-      audience='https://fullstackboilerplate/api'
+      audience='https://vroom/api'
     >
       <Provider store={store}>
         <Router>
