aaa2d1cbaabc8bb868d1220a797e229fd18cd170
Sophia-Lawler-Dormer <sophialawler@gmail.com>
2021-09-23 14:51:02 +1200

    user action test

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
       return knex('users').insert([
         {
           id: 1,
-          auth0_id: 'auth0|61414f84d35ac900717bc280',
-          name: 'kelmarna',
-          email: 'kelmarna@email.nz',
-          description: 'the awesome developer'
+          auth0_id: 'auth0|614bae6761718e0069e37cff',
+          name: 'kierantahir',
+          email: 'kierantahir@gmail.com',
+          description: 'admin'
         }
       ])
     })
