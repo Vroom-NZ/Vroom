857bf839e8dc95c6fe0efaa439e823aad9770f1e
kieran-tahir <kierantahir@gmail.com>
2021-09-24 16:32:17 +1200

    search-bar-updates

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer (props) {
   return (
     <footer className="footer">
-      <p>Vroom, 2021 (c) Contact Terms and Conditions</p>
+      <p>Vroom, 2021 &copy; Contact Terms and Conditions</p>
     </footer>
   )
 }
