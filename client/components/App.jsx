c10fc4ca6caa390af9b7212866248f66ef6592ba
Sophia-Lawler-Dormer <sophialawler@gmail.com>
2021-09-24 16:21:31 +1200

    links to post a ride

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
 import { cacheUser } from '../auth0-utils'
-import Nav from './Nav'
-import Registration from './Registration/Registration'
 import { Route } from 'react-router'
+
 import SearchBar from './SearchBar'
 import LandingPage from './LandingPage'
 import Footer from './Footer'
+import Registration from './Registration/Registration'
+import Nav from './Nav'
+import PostRide from './Drivers/PostRide'
 
 function App () {
   cacheUser(useAuth0)
@@ -18,9 +20,10 @@ function App () {
       {/* <Route exact path='/' component={Users} />
       <Route exact path='/' component={PingRoutes} /> */}
       <Route path='/register' component={Registration} />
+      <Route path='/drivers' component={PostRide} />
       <Route exact path='/' component={LandingPage}/>
       <Route exact path='/' component={SearchBar} />
-      <Route exact path='/' component={Footer} />
+      <Route path='/' component={Footer} />
     </div>
   )
 }
