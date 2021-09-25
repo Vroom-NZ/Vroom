854487cffc1a758c2e3d6f309cd24e9680826cbf
alexarlington <alexsarlington@gmail.com>
2021-09-25 16:29:23 +1200

    edited profile pages

---
+++ client/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProfileInfo () {
+  return (
+    <>
+      <div className="profile-container">
+        <p> Kia Ora hiihihihi </p>
+      </div>
+    </>
+  )
+}
+
+export default ProfileInfo
