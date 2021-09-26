5f741699987ed23b3a99465b09eeb8352eb4e68e
Sophia-Lawler-Dormer <sophialawler@gmail.com>
2021-09-27 11:16:54 +1300

    began profile stuff

--- client/apis/users.js
+++ client/apis/users.js
@@ -2,8 +2,8 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
-export function getUsers () {
-  return request.get(rootUrl + '/users')
+export function getUsers (id) {
+  return request.get(rootUrl + '/users' + `${id}`)
     .then(res => {
       return res.body.users
     })
