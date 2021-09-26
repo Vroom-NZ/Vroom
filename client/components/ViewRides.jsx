dbd7828847a0391383cca8a436b03f229ee43a4a
kieran-tahir <kierantahir@gmail.com>
2021-09-27 12:47:36 +1300

    search rides displays the ride you searched for

--- client/components/ViewRides.jsx
+++ client/components/ViewRides.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
-// import store from './../store'
-// import { getRides } from '../apis/rides'
-
-// // import { searchRides } from '../apis/rides'
-
-// function ViewRides (search, rides) {
-//   const getStore = store.getState(search)
-//   const searchStore = getStore.search
-//   console.log('getRides in viewRides: ', getRides(rides))
-//   // console.log('store.getState: ', searchStore)
-//   // console.log('store.getState: ', searchStore.startLocation)
-//   // console.log('viewrides - props: ', startLocation)
-//   return (
-//     <>
-//       <div className="view-rides-main-container">
-//         <div className='start-end-date-container'>
-//           <div className='route-container'>
-//             {searchStore.startLocation} to {searchStore.destination} for {searchStore.seatsAvailable} passengers
-//           </div>
-//           <div className='route-date-container'>
-//             {searchStore.date}
-//           </div>
-//         </div>
-//         <div className="view-results-container">
-//           1 2 3 4
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default connect()(ViewRides)
-
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 
@@ -48,8 +13,9 @@ function ViewRides (props) {
 
   useEffect(async () => {
     const postedRides = await getRides()
-    // const filteredRides = postedRides.filter((ride) => ride.startLocation === startLocation)
-    setRides(postedRides)
+    const filteredRides = postedRides.filter((ride) =>
+      (ride.startLocation === startLocation && ride.destination === destination && ride.date === date && ride.seatsAvailable >= seatsAvailable))
+    setRides(filteredRides)
   }, [])
 
   return (
