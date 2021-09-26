8f50e704c58fb92815118b78d7eebe08d7435c1a
Sophia-Lawler-Dormer <sophialawler@gmail.com>
2021-09-27 11:17:23 +1300

    profile mock data

--- client/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ client/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+imimport React from 'react'
 
-function ProfileInfo () {
+export default function profileInfo ({ user }) {
+
+  // const { firstName, lastName, email, phoneNumber, hasVehicle, bio, profilePic, carId } = user
+const mockUser = {
+  firstName: Sophia,
+  lastName: Lawler,
+  hasVehicle: true,
+  bio: "This is my bio wow so fun!",
+  profilePic: "https://media.npr.org/assets/img/2021/08/23/about_love-a5c5a160b609b952ef65d037dc214fe3dc8b692f-s800-c85.webp",
+  carId: 12
+}
+
+
   return (
     <>
       <div className="profile-container">
-        <p> Kia Ora hiihihihi </p>
+        <p> {firstName} {lastName} </p>
+        <p> </p>
+        <p></p>
+        {hasVehicle && 
+        <p> display car info</p> }
       </div>
     </>
   )
 }
-
-export default ProfileInfo
