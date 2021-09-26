d21f4f22c0eb073ba1357f00cfee68695583b774
kieran-tahir <kierantahir@gmail.com>
2021-09-27 12:37:37 +1300

    all rides display in view rides

--- client/components/ViewRides.jsx
+++ client/components/ViewRides.jsx
@@ -1,34 +1,87 @@
-import React from 'react'
+// import React from 'react'
+// import { connect } from 'react-redux'
+// import store from './../store'
+// import { getRides } from '../apis/rides'
+
+// // import { searchRides } from '../apis/rides'
+
+// function ViewRides (search, rides) {
+//   const getStore = store.getState(search)
+//   const searchStore = getStore.search
+//   console.log('getRides in viewRides: ', getRides(rides))
+//   // console.log('store.getState: ', searchStore)
+//   // console.log('store.getState: ', searchStore.startLocation)
+//   // console.log('viewrides - props: ', startLocation)
+//   return (
+//     <>
+//       <div className="view-rides-main-container">
+//         <div className='start-end-date-container'>
+//           <div className='route-container'>
+//             {searchStore.startLocation} to {searchStore.destination} for {searchStore.seatsAvailable} passengers
+//           </div>
+//           <div className='route-date-container'>
+//             {searchStore.date}
+//           </div>
+//         </div>
+//         <div className="view-results-container">
+//           1 2 3 4
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default connect()(ViewRides)
+
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import store from './../store'
+
 import { getRides } from '../apis/rides'
+import RideCard from './Profile/Rides/RideCard'
 
-// import { searchRides } from '../apis/rides'
+function ViewRides (props) {
+  const { startLocation, destination, date, seatsAvailable } = props.search
 
-function ViewRides (search, rides) {
-  const getStore = store.getState(search)
-  const searchStore = getStore.search
-  console.log('getRides in viewRides: ', getRides(rides))
-  // console.log('store.getState: ', searchStore)
-  // console.log('store.getState: ', searchStore.startLocation)
-  // console.log('viewrides - props: ', startLocation)
+  console.log('getRides in viewRides: ', props.search)
+
+  const [rides, setRides] = useState([])
+
+  useEffect(async () => {
+    const postedRides = await getRides()
+    // const filteredRides = postedRides.filter((ride) => ride.startLocation === startLocation)
+    setRides(postedRides)
+  }, [])
+
   return (
     <>
       <div className="view-rides-main-container">
         <div className='start-end-date-container'>
           <div className='route-container'>
-            {searchStore.startLocation} to {searchStore.destination} for {searchStore.seatsAvailable} passengers
+            {startLocation} to {destination} for {seatsAvailable} passengers
           </div>
           <div className='route-date-container'>
-            {searchStore.date}
+            {date}
           </div>
         </div>
         <div className="view-results-container">
-          1 2 3 4
+          {rides.length && (
+            <div>
+              {rides.map((ride) => {
+                console.log(ride)
+                return <RideCard key={ride.id} ride={ride} />
+              })}
+            </div>
+          )}
         </div>
       </div>
     </>
   )
 }
 
-export default connect()(ViewRides)
+function mapStateToProps (state) {
+  return {
+    search: state.search
+  }
+}
+
+export default connect(mapStateToProps)(ViewRides)
