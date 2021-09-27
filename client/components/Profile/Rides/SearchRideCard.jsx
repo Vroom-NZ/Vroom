b8a04cf0ea013d7c6f674912f33f0329a73d3a48
kieran-tahir <kierantahir@gmail.com>
2021-09-27 17:29:22 +1300

    view rides displays rides responsively wrapped

--- client/components/Profile/Rides/SearchRideCard.jsx
+++ client/components/Profile/Rides/SearchRideCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function SearchRideCard ({ ride }) {
   console.log('search ride result: ', ride)
-  const { date, arrivalTime, leavingTime, startLocation, destination, seatsAvailable, cost, passengers } = ride
+  const { date, arrivalTime, leavingTime, startLocation, destination, seatsAvailable, cost } = ride
 
   function handleSubmit () {
     console.log('book ride sends this info: ', ride)
@@ -11,14 +11,14 @@ export default function SearchRideCard ({ ride }) {
   return (
     <>
       <div className="each-ride-container">
-        <p> {startLocation}- {destination} </p>
-        <p>{date} {leavingTime}-{arrivalTime} </p>
-        <p> {cost}</p>
-        <p>{seatsAvailable}</p>
-        <p>You will be joining: </p>
-      </div>
-      <div className="bookRideButton">
-        <button className='orange-register-button animate__infinite' onClick={handleSubmit}>Book this ride</button>
+        {/* <p> Driving from <span className='bold-ride-info'>{startLocation}</span> to <span className='bold-ride-info'>{destination}</span> on <span className='bold-ride-info'>{date}</span>.</p> */}
+        <p> Leaving at <span className='bold-ride-info'>{leavingTime}</span> and planning on arriving at <span className='bold-ride-info'>{arrivalTime}</span>. </p>
+        <p> Cost: <span className='bold-ride-info'>${cost}</span></p>
+        <p> There are <span className='bold-ride-info'>{seatsAvailable}</span> seats available. </p>
+        <p>You will be joining: <span className='bold-ride-info'>Bob</span></p>
+        <div className="bookRideButton">
+          <button className='orange-register-button animate__infinite' onClick={handleSubmit}>Book this ride</button>
+        </div>
       </div>
     </>
   )
