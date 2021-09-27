e2d8aa9ad7d25e3c8457184a6d731cd276be9bc7
kieran-tahir <kierantahir@gmail.com>
2021-09-27 19:25:52 +1300

    styling and beginning of new db setup

--- client/components/Profile/Rides/SearchRideCard.jsx
+++ client/components/Profile/Rides/SearchRideCard.jsx
@@ -12,10 +12,15 @@ export default function SearchRideCard ({ ride }) {
     <>
       <div className="each-ride-container">
         {/* <p> Driving from <span className='bold-ride-info'>{startLocation}</span> to <span className='bold-ride-info'>{destination}</span> on <span className='bold-ride-info'>{date}</span>.</p> */}
-        <p> Leaving at <span className='bold-ride-info'>{leavingTime}</span> and planning on arriving at <span className='bold-ride-info'>{arrivalTime}</span>. </p>
-        <p> Cost: <span className='bold-ride-info'>${cost}</span></p>
-        <p> There are <span className='bold-ride-info'>{seatsAvailable}</span> seats available. </p>
-        <p>You will be joining: <span className='bold-ride-info'>Bob</span></p>
+        <div className='ride-card-profile-pic'>
+          <img src='images/Avatarprofpic.png'></img>
+        </div>
+        <div className='ride-card-info'>
+          <p> <span className='bold-ride-info'>Sean</span> is leaving at <span className='bold-ride-info'>{leavingTime}</span> and arriving at <span className='bold-ride-info'>{arrivalTime}</span>. </p>
+          <p> Cost: <span className='bold-ride-info'>${cost}</span> per person.</p>
+          <p> There are <span className='bold-ride-info'>{seatsAvailable}</span> seats available. </p>
+          <p>You will be joining: <span className='bold-ride-info'>Bob</span></p>
+        </div>
         <div className="bookRideButton">
           <button className='orange-register-button animate__infinite' onClick={handleSubmit}>Book this ride</button>
         </div>
