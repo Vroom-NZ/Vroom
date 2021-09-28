f0597ce68447c4adc11def2371f3ac0fb4a88e0f
kieran-tahir <kierantahir@gmail.com>
2021-09-28 14:19:55 +1300

    checkpoint

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function LandingPage () {
   return (
-    <div className='logo-container'>
-      <img src='images/VectorHeader.jpg' width='60%' height='auto'></img>
+    <div className='logo-container animate__zoomIn'>
+      <img src='images/VectorHeader.jpg' width='60%' height='auto' className='animate__zoomIn'></img>
     </div>
   )
 }
